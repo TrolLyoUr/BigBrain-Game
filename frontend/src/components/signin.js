d24379ac8082f4dee9984412a34e1e5b4144ea8b
Andy Kuo <stars0417@gmail.com>
2023-04-07 10:55:28 +1000

    class demo

---
+++ frontend/src/components/signin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Signin () {
+  return (
+    <>
+      Email: <input type="text" />
+      <br />
+      Password: <input type="password" />
+      <br />
+      <button>signin</button>
+    </>
+  )
+}
